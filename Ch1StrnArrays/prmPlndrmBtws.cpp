4be996599ce3f791d8146aab088cf870d15e1505
Kalyan Chavali <kalyan.chavali123@gmail.com>
2017-11-27 13:15:48 -0500

    Comments added

--- Ch1StrnArrays/prmPlndrmBtws.cpp
+++ Ch1StrnArrays/prmPlndrmBtws.cpp
@@ -8,13 +8,14 @@ int main(){
 	cin >> word;
 	int bitvec = 0;
 	bool flag = true;
-
+	// this is essentially counting number of characters in the string. setting the bits to zero means that the character appears even number of time. 1 means odd.
 	for (int i = 0; i < word.length(); ++i){
 		int index = word[i] - 'a';
 		int mask = 1 << index;
 		bitvec = bitvec ^ mask;
 	}
 
+	// if number of characters is even then the bitvec should be zero for the string to be a palindrom. Odd then only one bit should be set to 1 in the bitvec
 	if(word.length()%2==0){
 		if(bitvec!=0){
 			flag = false;
